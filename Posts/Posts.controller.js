8e9e1501bada41e75f58344ccfd7b15eb36f7b49
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-03-31 12:49:04 +0400

    feat(Posts): added services for controller

--- Posts/Posts.controller.js
+++ Posts/Posts.controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const Post = require("./Posts.model");
 
 class PostsController {
@@ -5,6 +6,57 @@ class PostsController {
     try {
       const { author, title, text, picture, date } = req.body;
       const post = await Post.create({ author, title, text, picture, date });
+      res.status(200).json(post);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find();
+      return res.status(200).json(posts);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async getOne(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        res.status(400).json({ message: "Please enter id of the post" });
+      }
+
+      const post = await Post.findById(id);
+      res.status(200).json(post);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const post = req.body;
+
+      if (!post._id) {
+        res.status(400).json({ message: "Please enter id of the post" });
+      }
+
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      res.status(200).json(updatedPost);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const post = await Post.findByIdAndDelete(id);
       res.status(200).json(post);
     } catch (e) {
       res.status(500).json(e.message);
--- Posts/Posts.router.js
+++ Posts/Posts.router.js
@@ -3,11 +3,9 @@ const Posts = require("./Posts.controller");
 
 const router = new Router();
 
-router.get("/posts", (req, res) => {
-  res.status(200).json(res);
-});
+router.get("/posts", Posts.getAll);
 router.post("/posts", Posts.create);
-router.get("/posts/:id");
-router.delete("/posts/:id");
+router.get("/posts/:id", Posts.getOne);
+router.delete("/posts/:id", Posts.delete);
 
 module.exports = router;
