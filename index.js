57e9a1a4d43248ddc12d09c32b6cd6ce1e377f21
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-03-31 10:52:16 +0400

    feat: use basic requests and responses

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+const PORT = 4200;
+
+app.use(express.json()); // for understanding json format
+
+app.get("/", (req, res) => {
+  //   res.send("Working!!!");
+  res.status(200).json(req.query);
+});
+
+app.post("/", (req, res) => {
+  res.status(200).json(req.body);
+});
+
+app.listen(PORT);
