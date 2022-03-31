93b957d42bfab4322d15bd92fd7787b6afd1a3e4
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-03-31 11:32:47 +0400

    feat: connected to the mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const PORT = 4200;
 
@@ -14,4 +16,14 @@ app.post("/", (req, res) => {
   res.status(200).json(req.body);
 });
 
-app.listen(PORT);
+async function connectionApp() {
+  try {
+    await mongoose.connect(process.env.APP_DB_URI);
+
+    app.listen(PORT);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+connectionApp();
